743e7a889ba57de9f953af94635036e4cd40c3f4
dharmik <dharmik.dudhat.ait@gmail.com>
2024-02-14 16:50:08 +0530

    user registration and login

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -2,7 +2,8 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import { JwtService } from '@nestjs/jwt';
-import { LoginUserDto } from './dto/auth-login.dto';
+
+
 
 
 @Injectable()
@@ -10,24 +11,22 @@ export class AuthService {
   constructor(
     private usersService: UserService,
     private jwtService: JwtService,
-  ) {}
+  ) { }
 
   // Login
-  async signIn(email: string, password: string): Promise<LoginUserDto> {
+  async signIn(email: string, password: string): Promise<string> {
     const user = await this.usersService.findOneUser(email);
-console.log(user)
+    console.log(user)
     if (user?.password !== password) {
       throw new UnauthorizedException({
         message: 'Please check your credentials',
       });
-    }
-    const payload = { sub: user.id, username: user.username };
-    return {
-      isError: false,
-      message: 'Login Successful',
-      data: {
-        access_token: await this.jwtService.signAsync(payload),
-      },
-    };
-  }
+    } 
+      
+      const payload = { sub: user.id, username: user.username };
+      return await this.jwtService.signAsync(payload
+      )
+    
+  };
 }
+
