af05ee03d64b2da2af038f821a8d34981761fa2e
dharmik <dharmik.dudhat.ait@gmail.com>
2024-02-15 11:15:00 +0530

    nothing

--- e-commerce/src/route.jsx
+++ e-commerce/src/route.jsx
@@ -7,12 +7,13 @@ import Layout from "./components/Layout/Layout";
 import { SignInOne } from "./components/Login/login";
 import { SignUpOne } from "./components/Registration/registration";
 import Contact from "./components/ContactUs/Contact";
-import { Home } from "./components/Home/Home";
+// import { Home } from "./components/Home/Home";
+import ProductCard from "./components/ProductCard/ProductCard";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
-      <Route path="" element={<Home />} />
+      <Route path="" element={<ProductCard />} />
       <Route path="login" element={<SignInOne />} />
       <Route path="registration" element={<SignUpOne />} />
       <Route path="contact" element={<Contact />} />
