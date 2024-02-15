8652624d738001ae1ab85ebaafca81dfd2d58d35
dharmik <dharmik.dudhat.ait@gmail.com>
2024-02-15 10:35:03 +0530

    user registration and login frontend backend joining

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:5173', // replace with your frontend's origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // enable credentials (cookies, authorization headers, etc.)
+  });
+
   await app.listen(3000);
 }
 bootstrap();
