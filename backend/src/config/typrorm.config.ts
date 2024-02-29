5d8c1a244316a6b0f0f851719fa502784efc551e
Dhruvik91 <yashgondaliya04@gmail.com>
2024-02-29 12:19:27 +0530

    Change in Response Structure and Add .env file

---
+++ backend/src/config/typrorm.config.ts
@@ -0,0 +1,29 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
+import { ContactEntity } from 'src/contact/entities/contact.entity';
+import { LogEntity } from 'src/logging/entity/logging.entity';
+import { ProductEntity } from 'src/product/entities/product.entity';
+import { User } from 'src/user/entities/user.entity';
+
+export class TypeOrmConfig {
+  static getOrmConfig(configService: ConfigService) {
+    return {
+      type: 'postgres',
+      host: configService.get('DB_HOST'),
+      port: configService.get('DB_PORT'),
+      username: configService.get('DB_USERNAME'),
+      password: configService.get('DB_PASSWORD'),
+      database: configService.get('DB_DATABASE'),
+      entities: [User, ContactEntity, ProductEntity, LogEntity],
+      synchronize: true,
+    };
+  }
+}
+
+export const typeOrmConfig: TypeOrmModuleAsyncOptions = {
+  imports: [ConfigModule],
+  useFactory: async (configService: ConfigService): Promise<any> => {
+    TypeOrmConfig.getOrmConfig(configService);
+  },
+  inject: [ConfigService],
+};
