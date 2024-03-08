a44d966227581ec5cf9561f96aae631fff3ad2d0
Dhruvik91 <yashgondaliya04@gmail.com>
2024-03-08 09:55:05 +0530

    Fix Product List Page

--- e-commerce/src/features/Admin/modals/ProductListModel.jsx
+++ e-commerce/src/features/Admin/modals/ProductListModel.jsx
@@ -23,59 +23,65 @@ const ProductListModal = ({ isOpen, onClose, children }) => {
   }, []);
 
   return (
-    <div className="fixed inset-0 z-50 overflow-auto bg-black bg-opacity-75 backdrop-filter backdrop-blur-lg flex items-center justify-center">
-      <div className="bg-white bg-opacity-25 p-8 mx-auto rounded-lg backdrop-filter backdrop-blur-lg">
-        <div className="flex justify-end text-3xl text-white">
+    <div className="fixed inset-0 z-50 overflow-auto bg-gray-900 bg-opacity-75 backdrop-filter backdrop-blur-lg flex items-center justify-center">
+      <div className="bg-white bg-opacity-75 p-4 mx-auto rounded-lg backdrop-filter backdrop-blur-lg">
+        <div className="flex justify-end text-2xl text-gray-900">
           <button onClick={onClose}>&times;</button>
         </div>
         {children}
         <div className="max-conte">
-          <div className="text-center font-bold text-3xl mb-4 text-red-300">
+          <div className="text-center font-bold text-xl mb-2 text-red-500">
             <h1>Product Table</h1>
           </div>
-          <div className="table-container">
-            <table className="border border-white w-full">
-              <thead className="border justify-between border-white">
+          <div className="table-container overflow-x-auto">
+            <table className="border border-gray-500 w-full">
+              <thead className="border justify-between border-gray-500">
                 <tr>
-                  <th className="py-2 text-blue-200 px-4 border text-center border-gray-300">
+                  <th className="py-2 text-blue-500 px-4 border text-center border-gray-500">
+                    SR. No
+                  </th>
+                  <th className="py-2 text-blue-500 px-4 border border-gray-500">
                     Product Id
                   </th>
-                  <th className="py-2 text-blue-200 px-4 border border-gray-300">
+                  <th className="py-2 text-blue-500 px-4 border border-gray-500">
                     Product Name
                   </th>
-                  <th className="py-2 text-blue-200 px-4 border border-gray-300">
-                    Product Description
+                  <th className="py-2 text-blue-500 px-4 border border-gray-500">
+                    Description
                   </th>
-                  <th className="py-2 text-blue-200 px-4 border border-gray-300">
+                  <th className="py-2 text-blue-500 px-4 border border-gray-500">
                     Price
                   </th>
-                  <th className="py-2 text-blue-200 px-4 border border-gray-300">
+                  <th className="py-2 text-blue-500 px-4 border border-gray-500">
                     Quantity
                   </th>
-                  <th className="py-2 text-blue-200 px-4 border border-gray-300">
+                  <th className="py-2 text-blue-500 px-4 border border-gray-500">
                     Image Path
                   </th>
                 </tr>
               </thead>
-              <tbody className="border border-white">
+              <tbody className="border border-gray-500">
                 {products.map((product, index) => (
                   <tr key={index} className="border-b">
-                    <td className="py-2 text-lime-300 px-1 text-center border border-gray-300 hover:text-green-500">
+                    <td className="py-2 text-gray-700 px-2 text-center border border-gray-500">
+                      {index + 1}
+                    </td>
+                    <td className="py-2 text-gray-700 px-2 text-center border border-gray-500">
                       {product.id}
                     </td>
-                    <td className="py-2 text-lime-300 hover:text-green-500 px-1 text-center border border-gray-300">
+                    <td className="py-2 text-gray-700 px-2 border border-gray-500">
                       {product.name}
                     </td>
-                    <td className="py-2 text-lime-300 hover:text-green-500 px-1 text-center border border-gray-300">
+                    <td className="py-2 text-gray-700 px-2 border border-gray-500">
                       {product.description}
                     </td>
-                    <td className="py-2 text-lime-300 hover:text-green-500 px-1 text-center border border-gray-300">
+                    <td className="py-2 text-gray-700 px-2 border border-gray-500">
                       {product.price}
                     </td>
-                    <td className="py-2 text-lime-300 hover:text-green-500 px-1 text-center border border-gray-300">
+                    <td className="py-2 text-gray-700 px-2 border border-gray-500">
                       {product.quantity}
                     </td>
-                    <td className="py-2 text-lime-300 hover:text-green-500 px-1 text-center border border-gray-300">
+                    <td className="py-2 text-gray-700 px-2 border border-gray-500">
                       {product.imagePath}
                     </td>
                   </tr>
